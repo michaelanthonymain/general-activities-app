bba1f654d7f15f971b6e97191e53d15a00ea384c
Michael Main <michaelanthonymain@gmail.com>
2014-04-24 09:45:56 -0500

    added categories

--- ga-app/app/models/event.rb
+++ ga-app/app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ActiveRecord::Base
   has_many :comments
   has_many :attendees, through: :attendances, source: :user
   belongs_to :creator, class_name: "User", foreign_key: :user_id
+  belongs_to :category
 end
