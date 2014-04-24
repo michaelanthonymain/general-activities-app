3d4d10ec051c4c12056cde94c560d022b5b850ad
Jaime Schubert <jkschub@gmail.com>
2014-04-24 11:33:06 -0500

    edit page done

--- ga-app/app/controllers/events_controller.rb
+++ ga-app/app/controllers/events_controller.rb
@@ -16,11 +16,13 @@ class EventsController < ApplicationController
 	end
 
 	def edit
-		@event = Event.find(params[:id])
+		@event = Event.find_by_user_id_and_id(params[:user_id], params[:id])
 	end
 
 	def update
-		
+		@event_to_update = Event.find(params[:id])
+		@event_to_update.update(event_params)
+		redirect_to @event_to_update
 	end
 
 	def destroy
@@ -30,7 +32,7 @@ class EventsController < ApplicationController
 	private
 
 	def event_params
-		params.require(:event).permit(:name, :description, :category, 
+		params.require(:event).permit(:name, :description, :category_id, 
 																	:price, :user_id, :signup_start, 
 																	:signup_end, :event_start, :event_end,
 																	:uses_paypal)
