c9d1f1959496312d2314bc6f10011ba343b26b01
Amelia Padua <amelia.padua@gmail.com>
2014-04-25 16:21:14 -0500

    test recurring event new signup date

--- ga-app/app/models/event.rb
+++ ga-app/app/models/event.rb
@@ -7,7 +7,31 @@ class Event < ActiveRecord::Base
 
   def self.create_recurring_event(last_event)
 
-    event = Event.new(name: last_event.name)
+    if last_event.recurring_timeframe == "weekly"
+      event_start = last_event.event_start + 1.week
+      event_end = last_event.event_end + 1.week
+      signup_start = last_event.signup_start + 1.week
+      signup_end = last_event.signup_end + 1.week
+    elsif last_event.recurring_timeframe == "monthly"
+      event_start = last_event.event_start + 1.month
+      event_end = last_event.event_end + 1.month
+      signup_start = last_event.signup_start + 1.month
+      signup_end = last_event.signup_end + 1.month
+    end
+
+    event = Event.new(
+      name: last_event.name,
+      description: last_event.description,
+      price: last_event.price,
+      user_id: last_event.user_id,
+      uses_paypal: last_event.uses_paypal,
+      recurring_events: last_event.recurring_events,
+      recurring_timeframe: last_event.recurring_timeframe,
+      event_start: event_start,
+      event_end: event_end,
+      signup_start: signup_start,
+      signup_end: signup_end
+      )
 
   end
 
--- ga-app/spec/models/event_spec.rb
+++ ga-app/spec/models/event_spec.rb
@@ -23,14 +23,20 @@ describe Event do
   it { should have_db_column(:recurring_timeframe).of_type(:string) }
 
   context "creates a recurring event (copy of event)" do 
-
     let(:event) { FactoryGirl.build(:event)}
+
     it "returns a copy of the event" do 
       recurring_event = Event.create_recurring_event(event)
 
       expect(recurring_event.name).to eq "Test Event"
     end
 
+    it "returns event with signup_start forwarded the specified recurring timeframe" do 
+      recurring_event = Event.create_recurring_event(event)
+      puts "recurring event: #{recurring_event.inspect}"
+
+      expect(recurring_event.signup_start).to eq(Time.new(2014, 4, 26, 2, 30, 30) + 1.week)
+    end
   end
 
 end
