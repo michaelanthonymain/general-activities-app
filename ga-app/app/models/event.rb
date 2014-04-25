d4367df471a709e0ac6df360df80ad8e7b07a547
Michael Main <michaelanthonymain@gmail.com>
2014-04-25 14:56:23 -0500

    Models tested and associated. Nice work team.

--- ga-app/app/models/event.rb
+++ ga-app/app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
   has_many :attendances
   has_many :comments
   has_many :attendees, through: :attendances, source: :user
-  belongs_to :creator, class_name: "User"
+  belongs_to :creator, class_name: "User", foreign_key: :user_id
   belongs_to :category
 end
--- ga-app/spec/models/attendance_spec.rb
+++ ga-app/spec/models/attendance_spec.rb
@@ -1,11 +1,5 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-<<<<<<< HEAD
-# describe Attendance do
-#   it { should belong_to(:user)}
-#   it { should belong_to(:event)}
-# end
-=======
 describe Attendance do
 
   #Associations
@@ -17,4 +11,3 @@ describe Attendance do
   it { should have_db_column(:event_id).of_type(:integer) }
   it { should have_db_column(:is_paid).of_type(:boolean) }
 end
->>>>>>> e162bfa2413cc69cc8d73f50c12c2ed3597f0f6e
