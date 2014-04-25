7328fbb06db369d4bf44d576f10444585ca66122
Michael Main <michaelanthonymain@gmail.com>
2014-04-25 13:15:02 -0500

    Events tests.

--- ga-app/spec/models/event_spec.rb
+++ ga-app/spec/models/event_spec.rb
@@ -1,9 +1,24 @@
 require 'spec_helper'
 
 describe Event do
+  #Associations
   it { should have_many(:attendances)}
   it { should have_many(:comments)}
   it { should have_many(:attendees).through(:attendances).source(:user)}
   it { should belong_to(:creator).class_name('User')}
   it { should belong_to(:category)}
+
+  #Database Columns
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:description).of_type(:text) }
+  it { should have_db_column(:category_id).of_type(:integer) }
+  it { should have_db_column(:price).of_type(:decimal) }
+  it { should have_db_column(:user_id).of_type(:integer) }
+  it { should have_db_column(:signup_start).of_type(:datetime) }
+  it { should have_db_column(:signup_end).of_type(:datetime) }
+  it { should have_db_column(:event_start).of_type(:datetime) }
+  it { should have_db_column(:event_end).of_type(:datetime) }
+  it { should have_db_column(:uses_paypal).of_type(:boolean) }
+  it { should have_db_column(:recurring_events).of_type(:boolean) }
+  it { should have_db_column(:recurring_timeframe).of_type(:string) }
 end
